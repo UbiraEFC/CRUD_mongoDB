067928db848664e8ca04cec781d8424e640266a3
UbiraEFC <ubirata.efc@gmail.com>
2022-06-04 17:06:24 -0300

    CRUD utilizando mongoDB

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const routes = require('./routes');
+require('./database');
+
+const port = 3333;
+
+const app = express();
+
+app.use(express.json());
+app.use(routes);
+
+app.listen(port, (err) => {
+    if(err) console.error(err)
+
+    console.info(`Server Runnig on http://localhost:${port}`);
+});
